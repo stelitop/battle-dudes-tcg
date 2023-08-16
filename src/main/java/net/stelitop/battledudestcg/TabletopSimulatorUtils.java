af518bdf92535f7b8df59ce77a1c6a9961e45a28
Stiliyan Nanovski <S.I.Nanovski@student.tudelft.nl>
2023-08-16 16:12:51 +0300

    Display the card cost and stats on exported cards.

--- src/main/java/net/stelitop/battledudestcg/TabletopSimulatorUtils.java
+++ src/main/java/net/stelitop/battledudestcg/TabletopSimulatorUtils.java
@@ -1,6 +1,7 @@
 package net.stelitop.battledudestcg;
 
 import net.stelitop.battledudestcg.game.database.entities.cards.Card;
+import net.stelitop.battledudestcg.game.database.entities.cards.DudeCard;
 import net.stelitop.battledudestcg.game.database.entities.collection.CardDeck;
 import net.stelitop.battledudestcg.game.database.repositories.CardRepository;
 import net.stelitop.battledudestcg.game.database.repositories.DeckRepository;
@@ -18,6 +19,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Component
 public class TabletopSimulatorUtils implements ApplicationRunner {
@@ -30,8 +32,8 @@ public class TabletopSimulatorUtils implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         String deckName = "Frogchamp";
 
-//        saveDeckAsSpreadsheet(StreamSupport.stream(deckRepository.findAll().spliterator(), false)
-//                .filter(x -> x.getName().equals(deckName)).findFirst().get());
+        saveDeckAsSpreadsheet(StreamSupport.stream(deckRepository.findAll().spliterator(), false)
+                .filter(x -> x.getName().equals(deckName)).findFirst().get());
     }
 
     public void saveDeckAsSpreadsheet(CardDeck deck) throws IOException {
@@ -73,8 +75,8 @@ public class TabletopSimulatorUtils implements ApplicationRunner {
 
             Point position = new Point(50, 750); // Position of the text
             Scalar color = new Scalar(0, 0, 0); // Color of the text (BGR format)
-            int font = Imgproc.FONT_HERSHEY_SIMPLEX; // Font type
-            double fontScale = 1.5; // Font scale
+            int font = Imgproc.FONT_HERSHEY_COMPLEX; // Font type
+            double fontScale = 1; // Font scale
             int thickness = 2; // Thickness of the text
             int lineHeight = 43; // Vertical space between lines
 
@@ -84,12 +86,21 @@ public class TabletopSimulatorUtils implements ApplicationRunner {
                 Imgproc.putText(finalMat, parts.get(i), linePosition, font, fontScale, color, thickness);
             }
 
+            if (!card.getCost().isBlank()) {
+                String costText = "[" + card.getCost().replace(".", "o") + "]";
+                Imgproc.putText(finalMat, costText, new Point(25, 50), font, fontScale*1.5, color, thickness);
+            }
+            if (card instanceof DudeCard dudeCard) {
+                String statsText = dudeCard.getHealth() + "/" + dudeCard.getOffense() + "/" + dudeCard.getDefence();
+                Imgproc.putText(finalMat, statsText, new Point(25, 975), font, fontScale*1.5, color, thickness);
+            }
+
             return finalMat;
         }
     }
 
     private List<String> splitTextIntoLines(String text) {
-        final int maxLength = 25;
+        final int maxLength = 35;
         List<String> ret = new ArrayList<>();
         StringBuilder sentence = new StringBuilder();
         for (String word : text.split(" ")) {
